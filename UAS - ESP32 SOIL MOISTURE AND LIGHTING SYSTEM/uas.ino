2b8603389644cac265976e7a679a7a19a1d6ed08
unknown <gilbertkristian79@gmail.com>
2026-01-02 23:44:01 +0700

    feat: integrate to integrator via ESP-NOW

--- UAS - ESP32 SOIL MOISTURE AND LIGHTING SYSTEM/uas.ino
+++ UAS - ESP32 SOIL MOISTURE AND LIGHTING SYSTEM/uas.ino
@@ -1,18 +1,29 @@
 #include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
-// ===== PIN =====
+// ===== PIN & THRESHOLD =====
 #define LDR_PIN   35
 #define SOIL_PIN  34
 #define LED_PIN   26
 #define PUMP_PIN  27
-
-// ===== THRESHOLD =====
 #define LIGHT_THRESHOLD  80  
 #define SOIL_THRESHOLD   2300 
 
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+typedef struct struct_message {
+    int lightIntensity;
+    int soilMoisture;
+    bool isDark;
+    bool isDry;
+} struct_message;
+
+struct_message myData;
+esp_now_peer_info_t peerInfo;
+
 volatile int lightIntensity = 0;
 volatile int soilMoisture   = 0;
-
 volatile bool isDark  = false;
 volatile bool isDry   = false;
 
@@ -20,10 +31,8 @@ void taskLightSensor(void *parameter) {
   while (1) {
     lightIntensity = analogRead(LDR_PIN);
     isDark = (lightIntensity > LIGHT_THRESHOLD);
-
     Serial.print("[LIGHT] Intensity: ");
     Serial.println(lightIntensity);
-
     vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
@@ -32,48 +41,59 @@ void taskSoilSensor(void *parameter) {
   while (1) {
     soilMoisture = analogRead(SOIL_PIN);
     isDry = (soilMoisture > SOIL_THRESHOLD);
-
     Serial.print("[SOIL] Moisture: ");
     Serial.println(soilMoisture);
-
     vTaskDelay(700 / portTICK_PERIOD_MS);
   }
 }
 
-// ===== TASK KONTROL AKTUATOR =====
 void taskControl(void *parameter) {
   while (1) {
     if (isDark) {
       digitalWrite(LED_PIN, HIGH);
-      Serial.println("[CTRL] GELAP -> LED ON");
     } else {
       digitalWrite(LED_PIN, LOW);
-      Serial.println("[CTRL] TERANG -> LED OFF");
     }
 
     if (isDry) {
-      digitalWrite(PUMP_PIN, LOW);
-      Serial.println("[CTRL] TANAH KERING -> POMPA ON");
+      digitalWrite(PUMP_PIN, LOW); 
     } else {
-      digitalWrite(PUMP_PIN, HIGH);
-      Serial.println("[CTRL] TANAH LEMBAB -> POMPA OFF");
+      digitalWrite(PUMP_PIN, HIGH); 
     }
 
+    myData.lightIntensity = lightIntensity;
+    myData.soilMoisture = soilMoisture;
+    myData.isDark = isDark;
+    myData.isDry = isDry;
+
+    esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
     vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
   pinMode(PUMP_PIN, OUTPUT);
-
   analogSetAttenuation(ADC_11db);
+
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("#G4G4L menginisialisasi ESP-NOW!");
+    return;
+  }
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Gagal nambahin peer");
+    return;
+  }
 
   xTaskCreate(taskLightSensor, "Light Task", 2048, NULL, 1, NULL);
   xTaskCreate(taskSoilSensor,  "Soil Task",  2048, NULL, 1, NULL);
-  xTaskCreate(taskControl,     "Control",    2048, NULL, 2, NULL);
+  xTaskCreate(taskControl,      "Control",    2048, NULL, 2, NULL);
 }
 
 void loop() {
